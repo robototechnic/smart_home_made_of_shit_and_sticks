e12750092536cfc80d08322892e360242dc8bcab
robototechnic <anon@post.su>
2018-08-22 01:22:00 +0300

    Changing the encryption logic. Data is transmitted in encrypted form. Added tracking of the desynchronization. Automatic sync added.

--- reciver/reciver.ino
+++ reciver/reciver.ino
@@ -4,23 +4,27 @@
 #include <Keeloq.h>
 
 EasyTransferVirtualWire ET;
-Keeloq k(0x01320334,0x05063708);      //keys
+Keeloq k(0x01320334,0x05063708);      // keys
 
-const unsigned int this_device_id = 1111; //unsigned int
+const byte this_device_id = 1;
+const byte count_of_transmitters = 7;
+unsigned int transmitter_id[count_of_transmitters] = {2222, 2, 3, 4, 5, 6, 7};     // array with id's of known transmitters. It is better to put first identifier of transmitter, which sends information often
+unsigned int packet_id[count_of_transmitters];
+bool b = false;       // trigger for synchronization
 
 struct SEND_DATA_STRUCTURE {      // structure with information for transmit
-  byte source_id;     // which device transmits the signal, max 255 devices
-  unsigned int destination_id;     // which device receive the signal
+  byte receiver_id;     // which device receive the signal, max 255
   unsigned int packet_id;     // package number
-  unsigned int random_count;      // it is necessary for the identical data after encryption to be different
+  unsigned int random_number;      // discriminator it is necessary for the identical data after encryption to be different
   byte command;     // command number
-  unsigned int data;      // unencrypted data
+  unsigned long data;      // unencrypted data
   unsigned long crypt;      // encrypted data
 } mydata;     // structure variable
 
 void setup() {
   pinMode(13, OUTPUT);      // 13 - built-in led
   Serial.begin(9600);
+  Serial.println(" ");
   ET.begin(details(mydata));
   vw_set_rx_pin(2);     // to which contact the reciver is connected
   vw_setup(2000);     // speed bits per second
@@ -29,48 +33,44 @@ void setup() {
 
 void loop() {
   if (ET.receiveData()) {
-    //if (mydata.destination_id == 0 || mydata.destination_id == 1) {     // 0 - broadcast, 1 - this device id
-    if (mydata.destination_id == this_device_id) {
+    if (mydata.receiver_id == 0 || mydata.receiver_id == this_device_id) {     // 0 - broadcast, or this device id
       digitalWrite(13, HIGH);      // 13 - built-in led, led on
-//      Serial.print("Encrypted data: ");
-//      Serial.print(mydata.crypt);
-      mydata.crypt = k.decrypt(mydata.crypt);     // decrypt the data
-//      Serial.print(" Decrypted data: ");
-//      Serial.println(mydata.crypt);
-      unsigned int random_count = floor((mydata.crypt - mydata.destination_id) / 100000);
-      if (random_count == mydata.random_count) {
-        unsigned int received_id = mydata.crypt - random_count * 100000;
-        if (received_id == this_device_id) {
-          Serial.print("Destination ID: ");
-          Serial.print(received_id);
-          Serial.print(" Source ID: ");
-          Serial.print(mydata.source_id); 
-          Serial.print(" Packet ID: ");
-          Serial.print(mydata.packet_id);
-          Serial.print(" Command: ");
-          Serial.print(mydata.command);
-          Serial.print(" Data: ");
-          if (mydata.command == 0) {      // battery charge data
-            Serial.println((float)mydata.data / 10, 1);     // float
-          } else Serial.println(mydata.data);      // other data, integer
+      mydata.crypt = k.decrypt(mydata.crypt);     // decrypt data
+      unsigned int random_number = floor((mydata.crypt - mydata.receiver_id) / 100000);     // calculate a random number
+      if (random_number == mydata.random_number) {      // compare with an unencrypted number
+        unsigned int obtained_id = mydata.crypt - random_number * 100000;     // calculate obtained id of transmitter
+        for (byte i = 0; i < count_of_transmitters; i++) {     // verify transmitter id
+          if (transmitter_id[i] == obtained_id) {
+            if (packet_id[i] == mydata.packet_id) {
+              packet_id[i]++;
+              mydata.data = k.decrypt(mydata.data);
+              Serial.print("Receiver ID: ");
+              Serial.print(mydata.receiver_id);
+              Serial.print(" Transmitter ID: ");
+              Serial.print(transmitter_id[i]); 
+              Serial.print(" Packet ID: ");
+              Serial.print(mydata.packet_id);
+              Serial.print(" Command: ");
+              Serial.print(mydata.command);
+              Serial.print(" Data: ");
+              if (mydata.command == 0) {      // battery charge data
+                Serial.println((float)mydata.data / 10, 1);     // float
+              } else Serial.println(mydata.data);      // other data, integer
+            } else {      // sync packet
+              if (b == false) {     // first time, ignore
+                b = true;
+                Serial.println("Desync!");
+              } else {      // second time, syncing. Everything will be ok on the third time
+                b = false;
+                Serial.println("Try to syncing...");
+                packet_id[i] = mydata.packet_id;
+                packet_id[i]++;
+              }
+            }
+            break;
+          } else Serial.println("Transmitter ID mismatch!");
         }
-        else {
-          Serial.println("Decrypted ID mismatch!");
-//          Serial.print("This device id: "); 
-//          Serial.println(this_device_id);
-//          Serial.print("Calculated destination id: ");
-//          Serial.println(received_id);
-//          Serial.println();          
-        }
-      }
-      else {
-        Serial.println("Random count mismatch!");
-//        Serial.print("Recived random count: "); 
-//        Serial.println(mydata.random_count);
-//        Serial.print("Calculated random count: ");
-//        Serial.println(random_count);
-//        Serial.println();
-      }
+      } else Serial.println("Random number mismatch!");
       digitalWrite(13, LOW);      // 13 - built-in led, led off
     }
   }
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -9,19 +9,18 @@
 #define DHTTYPE DHT22
 
 EasyTransferVirtualWire ET;
-Keeloq k(0x01320334,0x05063708);      //keys
+Keeloq k(0x01320334,0x05063708);      // keys
 DHT dht(DHTPIN, DHTTYPE);
 
-//unsigned int packet_id = 40000;
 int last_data[4];     // array with the latest data, count of cells = count of commands
+unsigned int transmitter_id = 2222;     // this device id, max 65535
 
 struct SEND_DATA_STRUCTURE {      // structure with information for transmit
-  byte source_id = 1;     // this device id, max 255 devices
-  unsigned int destination_id = 1111;     // which device receive the signal
+  byte receiver_id = 1;     // which device receive the signal, max 255
   unsigned int packet_id;     // package number
-  unsigned int random_count;      // it is necessary for the identical data after encryption to be different
+  unsigned int random_number;      // it is necessary for the identical data after encryption to be different
   byte command;     // command number: 0 - supply voltage; 1 - temperature; etc...
-  unsigned int data;      // unencrypted data
+  unsigned long data;      // unencrypted data
   unsigned long crypt;      // encrypted data
 } mydata;     // structure variable
 
@@ -44,9 +43,10 @@ int Transmitting(byte command, int data) {
     last_data[command] = data;
     mydata.command = command;
     mydata.data = data;
-    mydata.random_count = random(1, 42000);     // unsigned long can't make more than that!
-    unsigned long raw = mydata.random_count * 100000 + mydata.destination_id;
+    mydata.random_number = random(1, 42000);     // unsigned long can't make more than that!
+    unsigned long raw = mydata.random_number * 100000 + transmitter_id;
     mydata.crypt = k.encrypt(raw);
+    mydata.data = k.encrypt(mydata.data);
     //digitalWrite(13, HIGH);      // 13 - built-in led, led on
     ET.sendData();
     //digitalWrite(13, LOW);      // 13 - built-in led, led off
@@ -65,9 +65,14 @@ void setup() {
   ET.begin(details(mydata));
   analogReference(INTERNAL);
   randomSeed(analogRead(0));
+  for (byte i = 0; i < 3; i++) {      // cycle fo sincing transmitter and reciver
+    Transmitting (0, i);      // send nothing
+  }
 }
 
-void loop() {
+
+
+void loop() {     // commands with data 
   Transmitting (0, round((float)readVcc()/1000*10));      // battery charge
   Transmitting (1, round((float)dht.readTemperature()));
   Transmitting (2, round((float)dht.readHumidity()));
