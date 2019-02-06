d3790a9f9fd8403f4642d5e2abf78c78fb3134b8
robototechnic <robototechnic@github.com>
2019-02-06 20:59:44 +0300

    pulse

--- reciver/reciver.ino
+++ reciver/reciver.ino
@@ -11,18 +11,21 @@ const byte count_of_transmitters = 7;
 unsigned int transmitter_id[count_of_transmitters] = {2222, 2221, 3, 4, 5, 6, 7};     // array with id's of known transmitters. It is better to put first identifier of transmitter, which sends information often
 unsigned int packet_id[count_of_transmitters];
 bool b = false;       // trigger for synchronization
+const byte pin_power = 5;     // pin for sensor and transmitter power supply
 
 struct SEND_DATA_STRUCTURE {      // structure with information for transmit
   byte receiver_id;     // which device receive the signal, max 255
   unsigned int packet_id;     // package number
   unsigned int random_number;      // discriminator it is necessary for the identical data after encryption to be different
   byte command;     // command number
-  unsigned long data;      // unencrypted data
-  unsigned long crypt;      // encrypted data
+  unsigned long data;      // useful data
+  unsigned long service;      // service data
 } mydata;     // structure variable
 
 void setup() {
   pinMode(13, OUTPUT);      // 13 - built-in led
+  pinMode(pin_power, OUTPUT);
+  digitalWrite(pin_power, LOW);
   Serial.begin(9600);
   Serial.println(" ");
   ET.begin(details(mydata));
@@ -35,13 +38,13 @@ void loop() {
   if (ET.receiveData()) {
     if (mydata.receiver_id == 0 || mydata.receiver_id == this_device_id) {     // 0 - broadcast, or this device id
       digitalWrite(13, HIGH);      // 13 - built-in led, led on
-      mydata.crypt = k.decrypt(mydata.crypt);     // decrypt data
-      unsigned int random_number = floor((mydata.crypt - mydata.receiver_id) / 100000);     // calculate a random number
+      mydata.service = k.decrypt(mydata.service);     // decrypt service data
+      unsigned int random_number = floor((mydata.service - mydata.receiver_id) / 100000);     // calculate a random number
       if (random_number == mydata.random_number) {      // compare with an unencrypted number
-        unsigned int obtained_id = mydata.crypt - random_number * 100000;     // calculate obtained id of transmitter
+        unsigned int obtained_id = mydata.service - random_number * 100000;     // calculate obtained id of transmitter
         for (byte i = 0; i < count_of_transmitters; i++) {     // verify transmitter id
-          if (transmitter_id[i] == obtained_id) {
-            if (packet_id[i] == mydata.packet_id) {
+          if (transmitter_id[i] == obtained_id) {     // verify id of transmitter
+            if (packet_id[i] == mydata.packet_id) {     // verify synchronization
               packet_id[i]++;
               mydata.data = k.decrypt(mydata.data);
               Serial.print("Receiver ID: ");
@@ -55,10 +58,12 @@ void loop() {
               Serial.print(" Data: ");
               if (mydata.command == 2) {      // battery charge data
                 Serial.println((float)mydata.data / 10, 1);     // float
+              } else if (mydata.command == 6) {      // co2 data
+                Serial.println(mydata.data * 10);     // float
               } else Serial.println(mydata.data);      // other data, integer
-            } else {      // sync packet
-              if (b == false) {     // first time, ignore
-                b = true;
+            } else {      // synchronizing packets
+              if (b == false) {     // first time, ignore packet
+                b = true;     // trigger
                 //Serial.println("Desync!");
               } else {      // second time, syncing. Everything will be ok on the third time
                 b = false;
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,34 +1,38 @@
 // transmitter
-#include <VirtualWire.h> 
-#include <EasyTransferVirtualWire.h> 
+#include <VirtualWire.h>
+#include <EasyTransferVirtualWire.h>
 #include <Keeloq.h>
 #include <LowPower.h>
 #include <DHT.h>
-//#include <MQ135.h>
 #define DHTPIN 4     // to which contact the temperature sensor is connected
 #define DHTTYPE DHT22
 #define analogPin A0     // pin for reading random numbers
 #define interruptPin INT0     // interrupt 0 = 2 pin on arduino pro mini
-//#define CO2Pin A1     // to which contact the CO2 sensor is connected
 
 EasyTransferVirtualWire ET;
-Keeloq k(0x01320334,0x05063708);     // keys
+Keeloq k(0x01320334, 0x05063708);    // keys
 DHT dht(DHTPIN, DHTTYPE);
-//MQ135 gasSensor = MQ135(CO2Pin);
 
-const byte pin_transmitter = 3;
-const byte pin_power = 5;
+byte pin_hc_sr501 = 2;     // to which contact the motion sensor is connected
+const byte pin_transmitter = 3;     // to which contact the transmitter is connected
+const byte pin_power = 5;     // pin for sensor and transmitter power supply
 const byte pin_led = 13;     // 13 - built-in led
-int last_data[7];     // array with the latest data, count of cells = count of commands
+int last_data[8];     // array with the latest data, count of cells = count of commands
 unsigned int transmitter_id = 2221;     // this device id, max 65535
+
+byte request[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
+byte calib[9] = {0xFF,0x01,0x87,0x00,0x00,0x00,0x00,0x00,0x78};
+byte abcon[9] = {0xFF,0x01,0x79,0xA0,0x00,0x00,0x00,0x00,0xE6};
+byte abcoff[9] = {0xFF,0x01,0x79,0x00,0x00,0x00,0x00,0x00,0x86};
+unsigned char response[9];
 
 struct SEND_DATA_STRUCTURE {     // structure with information for transmit
   byte receiver_id = 1;     // which device receive the signal, max 255
-  unsigned int packet_id;     // package number
+  unsigned int packet_id = 1;     // package number
   unsigned int random_number;     // it is necessary for the identical data after encryption to be different
   byte command;     // command number: 0 - supply voltage; 1 - temperature; etc...
-  unsigned long data;     // unencrypted data
-  unsigned long crypt;     // encrypted data
+  unsigned long data;     // useful data
+  unsigned long service;     // service data
 } mydata;     // structure variable
 
 long readVcc() {
@@ -37,64 +41,91 @@ long readVcc() {
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
   delay(200);     // wait for Vref to settle
   ADCSRA |= _BV(ADSC);     // start conversion
-  while (bit_is_set(ADCSRA,ADSC));     // measuring
-  byte low  = ADCL;     // must read ADCL first - it then locks ADCH  
+  while (bit_is_set(ADCSRA, ADSC));    // measuring
+  byte low  = ADCL;     // must read ADCL first - it then locks ADCH
   byte high = ADCH;     // unlocks both
-  long result = (high<<8) | low;
+  long result = (high << 8) | low;
   result = 1125300L / result;     // calculate Vcc (in mV); 1125300 = 1.1*1023*1000
   return result;     // Vcc in millivolts
 }
 
-int Transmitting(byte command, int data) {
-  if (last_data[command] != data || command == 1) {     // if the data is different from the latter, or a motion detection command
+void Transmitting(byte command, unsigned int data) {
+//  if (last_data[command] != data || command == 1) {     // if data is different from the last, or a motion detection command
     last_data[command] = data;
     mydata.command = command;
     mydata.data = data;
     mydata.random_number = random(1, 42000);     // unsigned long can't make more than that!
     unsigned long raw = mydata.random_number * 100000 + transmitter_id;
-    mydata.crypt = k.encrypt(raw);
+    mydata.service = k.encrypt(raw);
     mydata.data = k.encrypt(mydata.data);
-    digitalWrite(pin_led, HIGH);     // led on
+//    digitalWrite(pin_led, HIGH);     // led on
     ET.sendData();
-    digitalWrite(pin_led, LOW);     // led off
+//    digitalWrite(pin_led, LOW);     // led off
     mydata.packet_id++;
-  }
+//  }
   delay(random(1000, 2000));
 }
 
-void setup() { 
-  //Serial.begin(9600);
-  //Serial.println(data);
+void setup() {
+  Serial.begin(9600);
+  //Serial.println(mydata.data);
   dht.begin();
   pinMode(pin_led, OUTPUT);
-  pinMode(pin_power, OUTPUT);     // pin for sensor and transmitter power supply
-  vw_set_tx_pin(pin_transmitter);     // to which contact the transmitter is connected
+  pinMode(pin_power, OUTPUT);
+  vw_set_tx_pin(pin_transmitter);
   vw_setup(2000);     // speed bits per second
   ET.begin(details(mydata));
   analogReference(INTERNAL);
   randomSeed(analogRead(analogPin));
-  digitalWrite(pin_power, HIGH);     // power up the sensor and transmitter
-  for (byte i = 0; i < 3; i++) {     // cycle fo sincing transmitter and reciver
-    Transmitting (0, i);     // send nothing
+  digitalWrite(pin_power, HIGH);
+  for (byte i = 0; i < 3; i++) {     // cycle for syncing transmitter and receiver
+    Transmitting(0, i);     // send nothing
   }
-  digitalWrite(pin_power, LOW);     // power down the sensor and transmitter
+  digitalWrite(pin_power, LOW);
 }
 
 void wakeUp() {     // wake up the controller
 }
 
+unsigned int co2() {
+  Serial.write(request, 9);
+  memset(response, 0, 9);
+  Serial.readBytes(response, 9);
+  byte i;
+  byte crc = 0;
+  for (i = 1; i < 8; i++) crc+=response[i];
+  crc = 255 - crc;
+  crc++;
+
+  if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
+    return 1;
+  } else {
+    unsigned int responseHigh = (unsigned int) response[2];
+    unsigned int responseLow = (unsigned int) response[3];
+    unsigned int ppm = (256*responseHigh) + responseLow;
+    return ppm;
+  }
+}
+
 void loop() {     // commands with data
-  digitalWrite(pin_power, HIGH);     // power up the sensor and transmitter
-  Transmitting (1, 1);     // motion detected
-  Transmitting (2, round((float)readVcc()/1000*10));     // battery charge
-  Transmitting (3, round((float)dht.readTemperature()));
-  Transmitting (4, round((float)dht.readHumidity()));
-  Transmitting (5, round((float)dht.computeHeatIndex((float)dht.readTemperature(), (float)dht.readHumidity(), false)));
-//  Transmitting (6, round((float)gasSensor.getPPM()));
-//  Transmitting (7, round((float)gasSensor.getRZero()));
-  digitalWrite(pin_power, LOW);     // power down the sensor and transmitter
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  for (byte i = 0; i <= 2; i++) {
+    digitalWrite(pin_power, HIGH);
+    Transmitting(1, digitalRead(pin_hc_sr501));     // motion detected
+    Transmitting(2, round((float)readVcc() / 100)); // battery charge
+    Transmitting(3, round((float)dht.readTemperature()));
+    Transmitting(4, round((float)dht.readHumidity()));
+    Transmitting(5, round((float)dht.computeHeatIndex((float)dht.readTemperature(), (float)dht.readHumidity(), false)));
+    Transmitting(6, round(co2() / 10));
+    digitalWrite(pin_power, LOW);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+  digitalWrite(pin_power, HIGH);
+  Transmitting(0, 10);
+  digitalWrite(pin_power, LOW);
   attachInterrupt(interruptPin, wakeUp, RISING);
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
   detachInterrupt(interruptPin);
+  digitalWrite(pin_power, HIGH);
+  Transmitting(7, 01);
+  digitalWrite(pin_power, LOW);
 }
